2a65d4020a7296bd8f602e11b98f82d5f44e81f3
meriemibrahim <meriem.ibrahim@esprit.tn>
2023-11-30 01:37:53 +0100

    script

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Ensure the script stops if any command fails
-set -e
+# Download and install Composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
 
-# Install Composer dependencies
-scoop install symfony-cli
+# Install project dependencies
 composer install --no-dev --optimize-autoloader
-
-
 
 # Other build commands if needed
